4c6828d7ffd2cbc9258ff0aed0f677bc44e340d1
Maaz Chowdhry <maazchowdhry2001@gmail.com>
2024-01-25 19:37:37 +0000

    Refactor code for placing orders and searching lessons

--- script.js
+++ script.js
@@ -64,21 +64,139 @@ const app = Vue.createApp({
             */
         },
         canAddToCart(id){
+            if (!this.lessons || this.lessons.length === 0 || !this.cart || id < 1 || id > this.lessons.length) {
+                return false; // Default value for handling invalid cases
+            }
             return this.lessons[id-1].availableInventory > this.cart[id];//Checking if the stock is available
         },
         stockLevel(id) {
+            if (!this.lessons || this.lessons.length === 0 || !this.cart || id < 1 || id > this.lessons.length) {
+                return 0; // Default value for handling invalid cases
+            }
             return this.lessons[id-1].availableInventory - this.cart[id];//Returning the stock level
         },
         clickCart() {
+            this.search_query = '';//Clearing the search query when the cart is clicked to show all the lessons in the cart
             this.showcart = !this.showcart;//Showing or hiding the cart
         },
         place_order() {
-            alert("Order placed!");//Alerting the user that the order has been placed
-            //reload the page
-            location.reload();
+            var sendOrder = {
+                "customerDetails": {
+                    "name": this.order.full_name,
+                    "contactNumber": this.order.phone_number
+                },
+                "orderItems": [],
+                "totalPrice": Number(this.cartTotal),
+                "orderDate": new Date().toISOString().slice(0, 10)
+            };
+        
+            // Process the cart items
+            for (let i = 1; i < this.cart.length; i++) {
+                if (this.cart[i] > 0) {
+                    sendOrder.orderItems.push({
+                        "lessonId": this.lessons[i-1]._id,
+                        "title": this.lessons[i-1].title,
+                        "price": this.lessons[i-1].price,
+                        "quantity": this.cart[i]
+                    });
+                }
+            }
+        
+            console.log(sendOrder);
+            // Send the order to the server
+            fetch(api_url + 'orders', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(sendOrder)
+            })
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                alert("Order placed!");//Alerting the user that the order has been placed
+                //reload the page
+                //location.reload();
+            })
+            .then(() => {//update the inventory of the lessons with the new stock level
+                for(let i = 1; i < this.cart.length; i++){
+                    if(this.cart[i] > 0){
+                        fetch(api_url + 'collections/lessons/'+this.lessons[i-1]._id, {
+                            method: 'PUT',
+                            headers: {
+                                'Content-Type': 'application/json'
+                            },
+                            body: JSON.stringify({
+                                "availableInventory": this.lessons[i-1].availableInventory - this.cart[i]
+                            })
+                        })
+                        .then(response => response.json())
+                        .then(data => {
+                            console.log(data);
+                        })
+                        .catch(err => {
+                            console.log(err);
+                        });
+                    }
+                }
+            })
+            .catch(err => {
+                console.log(err);
+            });
+
+            /*
+            var sendOrder = {
+                "customerDetails": {
+                    "name": this.order.full_name,
+                    "contactNumber": this.order.phone_number
+                },
+                "orderItems": {
+
+                },
+                "totalPrice": this.cartTotal,
+                "orderDate": new Date().toISOString().slice(0, 10)
+            }.then(() => {
+                for(let i = 1; i < this.cart.length; i++){
+                    if(this.cart[i] > 0){
+                        sendOrder.orderItems.push(
+                            {
+                                "lessonId": this.lessons[i-1]._id,
+                                "title": this.lessons[i-1].title,
+                                "price": this.lessons[i-1].price,
+                                "quantity": this.cart[i]
+                            }
+                        );
+                    }
+                }
+            }).then(() => {
+                console.log(sendOrder);
+            });
+            */
+            
+            
+            /*Old code where just an alert was shown and the page was reloaded
+                alert("Order placed!");//Alerting the user that the order has been placed
+                //reload the page
+                location.reload();
+            */
         },
         getImage(path){
             return api_url +"images/"+ path;//Returning the image path
+        },
+        debounceSearch() {
+            if (this.debounce) clearTimeout(this.debounce);
+            this.debounce = setTimeout(() => {
+                this.searchLessons();
+            }, 300); // Adjust 300 to the desired debounce time in milliseconds
+        },
+        searchLessons() {
+            fetch(api_url + 'search/lessons/'+this.lessonsCount+'/id/asc?searchTerm='+this.search_query)
+                .then(response => response.json())
+                .then(data => {
+                    this.lessons = data;
+                }).catch(err => {
+                    console.log(err);
+                });
         }
     },
     computed: {
@@ -102,11 +220,14 @@ const app = Vue.createApp({
             return RegExp(/^[a-zA-Z ]+$/).test(this.order.full_name)&&RegExp(/^[0-9]+$/).test(this.order.phone_number);
         },
         filteredLessons() {//Filtering the lessons
-            return this.lessons.filter(lesson => {
-                return lesson.title.toLowerCase().includes(this.search_query.toLowerCase())
-                || lesson.description.toLowerCase().includes(this.search_query.toLowerCase())
-                || lesson.location.toLowerCase().includes(this.search_query.toLowerCase())
-            });
+            return this.lessons;//Returning all the lessons as the search is done in the backend
+            /*This was used for frontend filtering
+                return this.lessons.filter(lesson => {
+                    return lesson.title.toLowerCase().includes(this.search_query.toLowerCase())
+                    || lesson.description.toLowerCase().includes(this.search_query.toLowerCase())
+                    || lesson.location.toLowerCase().includes(this.search_query.toLowerCase())
+                });
+            */
         },
         sortMethod() {
             if(this.sort_desc)
@@ -168,7 +289,22 @@ const app = Vue.createApp({
         }
     },
     watch: {
-
+        search_query: function(newQuery, oldQuery) {
+            if (newQuery !== oldQuery) {
+                this.debounceSearch();
+            }
+        }
+        /*
+        search_query: function(){
+            fetch(api_url + 'search/lessons/'+this.lessonsCount+'/id/asc?searchTerm='+this.search_query)//Fetching the lessons
+                .then(response => response.json())
+                .then(data => {
+                    this.lessons = data;//Setting the lessons
+                }
+            ).catch(err => {
+                console.log(err);
+            });
+        }*/
     }
 });
 app.mount('#app');
